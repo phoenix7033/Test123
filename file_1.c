a5a1be0e9b3326ad8cfd87a84c14b951230d6dd4
phoenix7033 <60518400+phoenix7033@users.noreply.github.com>
2024-04-30 00:06:40 +0530

    Update file_1.c

--- file_1.c
+++ file_1.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = numb1 + numb2;      
+    sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
